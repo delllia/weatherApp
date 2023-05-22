64c5e8129753f1a6b303055f04fd232e5ffa4d18
DeliaTirla <61746068+delllia@users.noreply.github.com>
2023-05-22 11:48:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ const apiKey = 'edfbc4d4aea2bc664ade9c88aebc1b25';
 button.addEventListener('click', changeTheme);
 
 function changeTheme() { 
-    body.classList.toggle('dark');
-    if (button.textContent === 'Dark Theme') {
-        button.textContent = 'Light Theme';
-    }
-    else {
-        button.textContent = 'Dark Theme';
-    }
+    const toggleDark = body.classList.toggle('dark');
+    // if (button.textContent === 'Dark Theme') {
+    //     button.textContent = 'Light Theme';
+    // }
+    // else {
+    //     button.textContent = 'Dark Theme';
+    // }
+    button.textContent = toggleDark ? 'Light Theme' : 'Dark Theme';
 }
 
 if (navigator.geolocation) {
@@ -31,6 +32,7 @@ function showPosition(position) {
     .then(response => response.json())
       .then(data => {
           const { name } = data[0];
+          //console.log('reverse geolocating: ', name);
           const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apiKey}&units=metric`;
    
           getWeather(url);
@@ -38,7 +40,6 @@ function showPosition(position) {
       .catch(error => {
           weatherHTML = `<h5>No city or country with this name... </h5>`;
           weatherContainer.innerHTML = weatherHTML;
-          console.log('error: ', error);
       });
 }
 
@@ -54,10 +55,11 @@ function getWeather(url) {
     fetch(url)
     .then(response => response.json())
       .then(data => {
+          console.log(data);
           const { name, main: { temp, humidity }, weather: [{ description, icon }] } = data;
-          const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${name}&appid=${apiKey}`;
+          const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${name}&appid=${apiKey}&units=metric`;
           const forecast = function () {
-              console.log(name);
+              //console.log(name);
               showForecast(urlForecast, name);
           };
       let weatherHTML = `
@@ -75,45 +77,95 @@ function getWeather(url) {
       .catch(error => {
           weatherHTML = `<h5>No city or country with this name... </h5>`;
           weatherContainer.innerHTML = weatherHTML;
-          console.log('error ', error);
+          //console.log('error ', error);
       });
 }
 
 function showForecast(url, name) {
     weatherContainer.innerHTML = `
     <h2>${name}</h2>
+    <button class="w3-button dark" onclick="showToday()">Today</button>
     <p>5 days forecast</p>
     `;
     fetch(url)
         .then(response => response.json())
         .then(data => {
+            //console.log('data is: ', data);
             const days = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-            const today = new Date().getDay();
+            let today = new Date().getDay();
+            //console.log('today is: ', new Date().getDate());
+            const currentDay = new Date().getDate();
+
+                    const forecast5 = {};
+                    let forecastTime = [];
+                    let forecastDay = currentDay;
             
             for (const [index, details] of Object.entries(data)) {
-            
                 if (index === 'list') {
                     let daysCount = 0;
-
+                   
                     for (const [i, detail] of details.entries()) {
-                        if (i < 5) {
-                            let dayIndex = (today + daysCount) < 7 ? (today + daysCount) : (today + daysCount) - 7;
-                            let { main, description, icon } = detail.weather[0];
-                            let weatherHTML = `
-                          
-                            <h2>${days[dayIndex]}</h2>
-                            <img src="http://openweathermap.org/img/w/${icon}.png" alt="${description}">
-                            <p>${main}</p>
-                            `;
-                            let p = document.createElement("p");
-                            weatherForecast.append(p);
-                            p.innerHTML = weatherHTML;
-                            daysCount++;
-                        }
+                        console.log('detail : ', detail.dt_txt.slice(0, 10));
+                        
+                        let { main, description, icon } = detail.weather[0];
+                        let temp = detail.main.temp;
+                        //console.log(detail.main.temp);
+
+                        if (detail.dt_txt.slice(8, 10) == forecastDay) {
+                            //console.log(forecastDay);
+                            forecastTime.push(`${detail.dt_txt.slice(-9)}:${temp}:${icon}`);
+                            forecast5[detail.dt_txt.slice(0, 10)] = forecastTime;
+                            //console.log(forecastTime);
+                        } else {
+                            forecast5[detail.dt_txt.slice(0, 10)] = forecastTime;
+                            console.log(forecast5);
+                            //console.log(forecastDay);
+                            forecastDay++;
+                            //console.log(forecastDay);
+                            forecastTime = [];
+                            forecastTime.push(`${detail.dt_txt.slice(-9)}:${temp}:${icon}`);
+                            
+                         }
                     }
+
+                   
+                    let forecast5Arr = Object.entries(forecast5);
+                    forecast5Arr.pop();
+                    
+                    for (const [date, elements] of forecast5Arr) {
+                        //console.log(forecast5);
+                        console.log(date);
+                        let dayIndex = (today + daysCount) < 7 ? (today + daysCount) : (today + daysCount) - 7;
+                        let weatherHTML = `
+                            <h3>${days[dayIndex]}</h3>
+                            <h5>${date}</h5>
+                            `;
+                        let p = document.createElement('p');
+                        p.classList.add(`head${date}`);
+                        weatherForecast.append(p);
+                        p.innerHTML = weatherHTML;
+                        
+                        for (let el of elements) {
+                            let temperature = Math.round(Number(el.split(':')[3]));
+                            let icon = el.split(':')[4];
+                            //console.log(el);
+                            let span = document.createElement("span");
+                            span.innerHTML = `${el.slice(0,6)} ⌚ / <strong>${temperature}°C</strong><img src="http://openweathermap.org/img/w/${icon}.png" alt="desc">`;
+                            document.querySelector(`.head${date}`).append(span);
+                        }
+                        daysCount++;
+                    }
+                    
                 }
                 
             }
         })
         .catch(error => console.log(error));
+}
+
+function showToday() {
+    let searchTerm = document.querySelector('h2').textContent;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${apiKey}&units=metric`;
+    getWeather(url);
+    document.querySelector('#weatherForecast').innerHTML = '';
 }
